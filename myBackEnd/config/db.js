990c4ab9d0c09ac95e24050cc4d4a48f3f7613c3
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-01-26 19:50:09 -0500

    Refactor database connection to use createConnection and update Vercel configuration

--- myBackEnd/config/db.js
+++ myBackEnd/config/db.js
@@ -1,14 +1,19 @@
-require('dotenv').config();
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    connectionLimit: 10,
-    waitForConnections: true,
+    port: process.env.DB_PORT || 59374
 });
 
-module.exports = connection = pool.promise();
+connection.connect(err => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err);
+        return;
+    }
+    console.log('Conectado a la base de datos');
+});
+
+module.exports = connection;
