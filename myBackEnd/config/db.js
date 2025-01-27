f2b8e98bc4455dc18cf9849386f766b7e13b2394
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-01-26 19:54:20 -0500

    Refactor database connection to use connection pool and improve configuration

--- myBackEnd/config/db.js
+++ myBackEnd/config/db.js
@@ -1,19 +1,14 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 59374
+    port: process.env.DB_PORT,
+    connectionLimit: 10,
+    waitForConnections: true,
 });
 
-connection.connect(err => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err);
-        return;
-    }
-    console.log('Conectado a la base de datos');
-});
-
-module.exports = connection;
+module.exports = pool.promise();
