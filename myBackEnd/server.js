53a95d5142417ca4531df2f1591b4776612b1e63
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-01-26 20:33:25 -0500

    Remove deprecated user controller and routes

--- myBackEnd/server.js
+++ myBackEnd/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
-const userRoutes2 = require('./routes/userRoutes2');
 const courseRoutes = require('./routes/courseRoutes');
 const authRoutes = require('./routes/authRoutes');
 const rolesRoutes = require('./routes/rolesRoute');
@@ -22,7 +21,6 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/users', userRoutes);
-app.use('/api/users2', userRoutes2);
 app.use('/api/courses', courseRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/roles', rolesRoutes);
