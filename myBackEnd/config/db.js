1f71d4da9b6b7c447355377c0eb72c6306df5387
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-01-26 20:57:50 -0500

    Update database configuration to use Clever Cloud MySQL addon

--- myBackEnd/config/db.js
+++ myBackEnd/config/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT || 3306, // Usar puerto por defecto si no está definido
     connectionLimit: 10,
     waitForConnections: true,
 });
