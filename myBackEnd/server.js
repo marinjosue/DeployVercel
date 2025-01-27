69b80ff09ae547e60df9f76d422cf040c1c980da
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-01-26 19:53:16 -0500

    Update CORS configuration to allow all origins and restructure Vercel configuration

--- myBackEnd/server.js
+++ myBackEnd/server.js
@@ -8,13 +8,15 @@ const rolesRoutes = require('./routes/rolesRoute');
 
 const app = express();
 
-// Configuración específica de CORS
-app.use(cors({
-    origin: 'https://awd-22962024-alpha-team.vercel.app', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+// Habilitar CORS para todas las rutas
+app.use(
+    cors({
+        origin: '*', // Permitir todas las solicitudes de cualquier dominio
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    })
+);
+
 // Middleware para parsear JSON
 app.use(express.json());
 
@@ -25,11 +27,10 @@ app.use('/api/courses', courseRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/roles', rolesRoutes);
 
-
-
 // Puerto del servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-module.sport= app;
+
+module.exports = app; 
