77eaed8e5113e402f45d776fd6f32ebd77d8db69
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-01-26 19:37:18 -0500

    Update server log message to remove hardcoded URL

--- myBackEnd/server.js
+++ myBackEnd/server.js
@@ -30,5 +30,5 @@ app.use('/api/roles', rolesRoutes);
 // Puerto del servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en ${PORT}`);
 });
